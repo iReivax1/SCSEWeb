664622815cdd902d7566b496a9dfbe7c34c6c06a
Xavier Tan <xavi0007@e.ntu.edu.sg>
2020-11-28 19:27:14 +0800

    bug squashed, clickable

--- src/components/MapComponents/MapPlacement.js
+++ src/components/MapComponents/MapPlacement.js
@@ -12,21 +12,52 @@ function MapPlacement() {
         <Pannellum
           width="100%"
           height="100vh"
-          image="/images/bookcube1.jpg"
+          image="/images/pano.jpg"
           pitch={10}
           yaw={180}
-          hfov={110}
+          hfov={500}
           autoLoad
-          showZoomCtrl={false}
+          author=""
+          title=""
+          orientationOnByDefault={false}
+          draggable
+          keyboardZoom
+          mouseZoom
+          preview=""
+          previewAuthor=""
+          previewTitle=""
+          showControls
+          showFullscreenCtrl
+          showZoomCtrl
+          onLoad={() => { console.log("panorama loaded"); }}
+          onScenechange={(id) => { console.log("Scene has change on " + id); }}
+          onScenechangefadedone={() => { console.log("panorama loaded"); }}
+          onError={(err) => { console.log("Error", err); }}
+          onErrorcleared={() => { console.log("Error Cleared"); }}
+          onMousedown={(evt) => { console.log("Mouse Down", evt); }}
+          onMouseup={(evt) => { console.log("Mouse Up", evt); }}
+          onTouchstart={(evt) => { console.log("Touch Start", evt); }}
+          onTouchend={(evt) => { console.log("Touch End", evt); }}
+          hotspotDebug={false}
         >
           <Pannellum.Hotspot
-            type="custom"
-            pitch={12.41}
-            yaw={117.76}
-            handleClick={(evt, name) => console.log(name)}
-            name="image info"
+            type="info"
+            pitch={11}
+            yaw={-167}
+            text="See courses"
+            URL="/all_courses"
           />
+{/* 
+          <Pannellum.Hotspot
+            type="custom"
+            pitch={31}
+            yaw={150}
+            handleClick={(evt, args) => this.hanldeClickImage(evt, args)}
+            handleClickArg={{ "name": "test" }}
+          /> */}
+
         </Pannellum>
+
       </div>
       <ButtonContainer>
         SWLAB
