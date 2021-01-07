9afe814e7b0e9061fe264bf8abffe95209d1d416
TanJunHong <b190013@e.ntu.edu.sg>
2021-01-08 01:08:12 +0800

    Tidy up MapPlacement

--- src/components/map_components/MapPlacement.js
+++ src/components/map_components/MapPlacement.js
@@ -5,82 +5,41 @@ import './MapPlacement.css';
 import L1Layout from "./L1Layout";
 import L2Layout from "./L2Layout";
 import B1Layout from './B1Layout';
-import B2Layout from './B2Layout';
+import B2Layout from "./B2Layout";
 
 
 export default class MapPlacement extends Component {
+    childPanoMap = {
+        "Level 2": <L2Layout/>,
+        "Level 1": <L1Layout/>,
+        "Basement 1": <B1Layout/>,
+        "Basement 2": <B2Layout/>,
+        //"Basement 3": <B3Layout/>,
+        //"Basement 4": <B4Layout/>
+    }
+
     constructor(props) {
         super(props);
         this.state = {
             layout: <L1Layout/>
         };
-        //this.changePanoLayout = React.createRef();
-        this.handleClick_l1 = this.handleClick_l1.bind(this);
-        this.handleClick_l2 = this.handleClick_l2.bind(this);
     }
 
-    handleClick_l1 = () => {
+    handleClick = (level) => {
         this.setState({
-            layout: <L1Layout/>
+            layout: this.childPanoMap[level]
         })
     }
-    handleClick_l2 = () => {
-        this.setState({
-            layout: <L2Layout/>
-        })
-
-    }
-    handleClick_b1 = () => {
-        this.setState({
-            layout: <B1Layout/>
-        })
-    }
-    handleClick_b2 = () => {
-        this.setState({
-            layout: <B2Layout/>
-        })
-    }
-    handleClick_b3 = () => {
-        this.setState({
-                layoutImage: ['/images/img-home.jpg'],
-                level: 'b3'
-            }
-        )
-    }
-    handleClick_b4 = () => {
-        this.setState({
-                layoutImage: ['/images/img-home.jpg'],
-                level: 'b4'
-            }
-        )
-    }
-
 
     render() {
         return (
             <React.Fragment>
                 <div className='layout-container'>
-                    {console.log(this.state)}
                     {this.state.layout}
                 </div>
-                <ButtonContainer onClick={() => this.handleClick_l2()}>
-                    Level 2
-                </ButtonContainer>
-                <ButtonContainer onClick={() => this.handleClick_l1()}>
-                    Level 1
-                </ButtonContainer>
-                <ButtonContainer onClick={() => this.handleClick_b1()}>
-                    Level B1
-                </ButtonContainer>
-                <ButtonContainer onClick={() => this.handleClick_b2()}>
-                    Level B2
-                </ButtonContainer>
-                <ButtonContainer onClick={() => this.handleClick_b3()}>
-                    Level B3
-                </ButtonContainer>
-                <ButtonContainer onClick={() => this.handleClick_b4()}>
-                    Level B4
-                </ButtonContainer>
+                {Object.keys(this.childPanoMap).map((item) => <ButtonContainer onClick={() => this.handleClick(item)}>
+                    {item}
+                </ButtonContainer>)}
             </React.Fragment>
         )
     }
