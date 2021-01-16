1200666e9c8832ac37f8f2e5e8c0f76cd7d8eea2
TanJunHong <b190013@e.ntu.edu.sg>
2021-01-16 17:45:44 +0800

    Small refactor

--- src/components/video_components/VideoPage.js
+++ src/components/video_components/VideoPage.js
@@ -1,21 +1,21 @@
 import React, {Component} from "react";
+import "../../App.css";
 import Footer from "../Footer";
-import '../../App.css'
 import VideoPlacement from "./VideoPlacement";
 
 
 export default class VideoPage extends Component {
-    
-    
+
+
     render() {
         return (
             <React.Fragment>
                 <VideoPlacement vid="https://www.youtube.com/embed/9r0pLGWzi48"/>
-                <br></br>
+                <br/>
                 <VideoPlacement vid="https://www.youtube.com/embed/QtrtDtSqnBY"/>
-                <br></br>
+                <br/>
                 <VideoPlacement vid="https://www.youtube.com/embed/27C5lG7pe1Q"/>
-                <br></br>
+                <br/>
                 <VideoPlacement vid="https://www.youtube.com/embed/3U-Od0fG1ws"/>
                 <Footer/>
             </React.Fragment>
--- src/components/video_components/VideoPlacement.js
+++ src/components/video_components/VideoPlacement.js
@@ -2,11 +2,13 @@ import React from "react";
 import "../../App.css";
 import "./VideoPlacement.css";
 
-function VideoPlacement(props) { 
-  
+function VideoPlacement(props) {
+
     return (
         <div className="hero-container">
-            <iframe width="100%" height="100%" src={props.vid} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe title="video" width="100%" height="100%" src={props.vid} frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen/>
         </div>
     );
 }
