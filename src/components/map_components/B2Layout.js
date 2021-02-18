5a8d8a149c1dd3b8194643f78df3b0e7f88cb174
TanJunHong <b190013@e.ntu.edu.sg>
2021-02-18 19:39:16 +0800

    Fix B2

--- src/components/map_components/B2Layout.js
+++ src/components/map_components/B2Layout.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import ImageScroller from "react-image-scroller";
+import ReactJsAlert from "reactjs-alert";
 import {ButtonOverlay} from "../Button";
 import "./Layout.css";
 
@@ -9,7 +10,7 @@ export default class B2Layout extends Component {
         super(props);
         this.state = {
             levelData: [{
-                btnNames: ["Computer network and comms lab", "Parallel & distributed systems lab"],
+                btnNames: ["Computer network & comms lab", "Parallel & distributed systems lab"],
                 image: "/images/b2/map/a.jpg"
             },
                 {
@@ -60,11 +61,36 @@ export default class B2Layout extends Component {
     }
 
     handleToggleClick(event) {
-        const childPanoMap = {};
-        this.setState({
-            showPano: true
-        });
-        this.childPano = childPanoMap[event.target.id];
+        switch(event.target.id) {
+            case "Computer network & comms lab":
+                this.setState({
+                    showAlert: true,
+                    quote: "CNCL conducts research in data communications and networking that addresses information transfer among humans, computers, and devices using wired and wireless connections.",
+                    title: "Computer network & comms lab"
+                });
+                break;
+            case "Parallel & distributed systems lab":
+                this.setState({
+                    showAlert: true,
+                    quote: "PDCL (formerly Parallel and Distributed Computing Lab) was established in 1994. Since then, there have been numerous successful research projects and activities carried out.",
+                    title: "Parallel and Distributed Computing Lab",
+                });
+                break;
+            case "Hardware & Embedded Systems Lab":
+                this.setState({
+                    showAlert: true,
+                    quote: "HESL, originally known as Centre for High Performance Embedded Systems (CHiPES),was founded in April 1998 to promote research and development in embedded systems engineering using state-of-the-art VLSI CAD tools and technology.",
+                    title: "Hardware & Embedded Systems Lab",
+                });
+                break;
+            case "Cyber Security Lab":
+                this.setState({
+                    showAlert: true,
+                    quote: "Cyber Security Lab (CSL)",
+                    title: "Cyber Security Lab (CSL)",
+                });
+                break;
+        }
     }
 
     render() {
@@ -75,12 +101,12 @@ export default class B2Layout extends Component {
                 {item.btnNames.map(
                     (btnName, j) => <span key={j}>
                         {(btnName === "Computer network & comms lab" || btnName === "HPL") ?
-                            <ButtonOverlay className="first" id={btnName}
+                            <ButtonOverlay className="first" id={btnName} onClick={this.handleToggleClick}
                             > {btnName}</ButtonOverlay>
                             : (btnName === "Parallel & distributed systems lab") ?
-                                <ButtonOverlay className="third" id={btnName}
+                                <ButtonOverlay className="third" id={btnName} onClick={this.handleToggleClick}
                                 > {btnName}</ButtonOverlay>
-                                : <ButtonOverlay className="second" id={btnName}
+                                : <ButtonOverlay className="second" id={btnName} onClick={this.handleToggleClick}
                                 > {btnName}</ButtonOverlay>
                         }
 
@@ -93,6 +119,13 @@ export default class B2Layout extends Component {
 
         return (
             <div>
+                {this.state.showAlert ? <ReactJsAlert
+                    type="info"
+                    title={this.state.title}
+                    status={this.state.showAlert}
+                    quote={this.state.quote}
+                    Close={() => this.setState({showAlert: false})}
+                /> : null}
                 {
                     (this.state.showPano) ? loadPano : loadLayout
                 }
